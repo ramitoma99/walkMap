331dfda32bc2ceabe792b4b3789fe8edbb2d22a1
ramitoma99 <154519701+ramitoma99@users.noreply.github.com>
2024-05-28 22:33:33 +0200

    world cell and map tries

--- Cell.java
+++ Cell.java
@@ -1,6 +1,14 @@
 public class Cell {
 
     private char cellContent;
+    private int posX;
+    private int posY;
+
+    public Cell(char content, int x, int y){
+        this.cellContent = content;
+        this.posX = x;
+        this.posY = y;
+    }
 
     public char getCellContent(){
         return this.cellContent;
@@ -10,5 +18,20 @@ public class Cell {
         this.cellContent = new_content;
     }
 
-    
+    public int getPosX(){
+        return this.posX;
+    }
+
+    public void setPosX(int new_x){
+        this.posX = new_x;
+    }
+
+    public int getPosY(){
+        return this.posY;
+    }
+
+    public void setPosY(int new_y){
+        this.posY = new_y;
+    }
+
 }
--- World.java
+++ World.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
 
-    private int[][] worldMap;
-    private int size;
+    private int worldSize;
 
+    public World(int worldSize){
+        this.worldSize = worldSize;
+    }
 
+    int[][] worldMap;
+
+    public int getWorldSize(){
+        return this.worldSize;
+    }
+
+    private void layWorld(int worldSize){
+        for (int x = 0; x < worldSize; x++){
+            for (int y = 0; y < worldSize; y++){
+                worldMap[x][y] = new Cell('.', x, y);
+            }
+        }
+    }
 
 }
